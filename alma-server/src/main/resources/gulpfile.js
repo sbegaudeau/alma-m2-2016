a5673abbcb2f5a2a5be6fe8444241d49f9e22e28
Thao COURAUD <E128809K@S046V7pc17.ensinfo.sciences.univ-nantes.prive>
2016-12-08 17:15:31 +0100

    fix name organization

--- alma-server/src/main/resources/gulpfile.js
+++ alma-server/src/main/resources/gulpfile.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 <INSERT YOUR ORGANISATION NAME HERE>.
+ * Copyright (c) 2016 thaologie.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the MIT License which
  * accompanies this distribution, and is available at
